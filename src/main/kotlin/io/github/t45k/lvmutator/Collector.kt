1499b2b5eb8111b371d96600bfaaaba99d6e345e
T45K <tasktas9@gmail.com>
2020-12-24 13:11:23 +0900

    upd

--- src/main/kotlin/io/github/t45k/lvmutator/Collector.kt
+++ src/main/kotlin/io/github/t45k/lvmutator/Collector.kt
@@ -2,10 +2,14 @@ package io.github.t45k.lvmutator
 
 import org.eclipse.core.runtime.NullProgressMonitor
 import org.eclipse.jdt.core.dom.AST.JLS14
+import org.eclipse.jdt.core.dom.ASTNode
 import org.eclipse.jdt.core.dom.ASTParser
 import org.eclipse.jdt.core.dom.ASTVisitor
+import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor
 import org.eclipse.jdt.core.dom.CompilationUnit
 import org.eclipse.jdt.core.dom.MethodDeclaration
+import org.eclipse.jdt.core.dom.SimplePropertyDescriptor
+import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor
 import java.io.File
 import kotlin.random.Random
 
@@ -25,38 +29,51 @@ class Collector {
                 val compilationUnit = ASTParser.newParser(JLS14)
                     .also { it.setSource(javaFile.readText().toCharArray()) }
                     .let { it.createAST(NullProgressMonitor()) as CompilationUnit }
-                val visitor = MethodCollector()
+                val visitor = MethodCollector(compilationUnit, javaFile)
                 compilationUnit.accept(visitor)
                 visitor.getMethods()
             }
             .shuffled(rand)
             .take(100)
-            .forEachIndexed { index, methodDeclaration ->
-                File("fragment", "${index + 1}").writeText(methodDeclaration.toString())
+            .forEachIndexed { index, method ->
+                File("fragment", "${index + 1}").writeText(method)
             }
     }
 
-    class MethodCollector : ASTVisitor() {
-        private val methods: MutableList<MethodDeclaration> = mutableListOf()
-        fun getMethods(): List<MethodDeclaration> = methods
+    class MethodCollector(private val compilationUnit: CompilationUnit, private val javaFile: File) : ASTVisitor() {
+        private val methods: MutableList<String> = mutableListOf()
+        fun getMethods(): List<String> = methods
 
         override fun visit(node: MethodDeclaration?): Boolean {
-            node?.body?.also {
-                /*
-                val startLine = compilationUnit.getLineNumber(it.startPosition)
+            node?.also {
+                val startLine = if (node.javadoc == null) {
+                    compilationUnit.getLineNumber(node.startPosition)
+                } else {
+                    compilationUnit.getLineNumber(getNodeNextToJavaDoc(node).startPosition)
+                }
                 val endLine = compilationUnit.getLineNumber(it.startPosition + it.length)
                 val loc = endLine - startLine + 1
                 if (loc in MIN..MAX) {
                     node.javadoc = null
-                    methods.add(node)
-                }
-                 */
-                if (it.toString().split("\n").size in MIN..MAX) {
-                    node.javadoc = null
-                    methods.add(node)
+                    methods.add(javaFile.readLines().subList(startLine - 1, endLine).joinToString("\n"))
                 }
             }
             return false
         }
+
+        @Suppress("UNCHECKED_CAST")
+        private fun getNodeNextToJavaDoc(node: MethodDeclaration): ASTNode =
+            (node.structuralPropertiesForType() as List<StructuralPropertyDescriptor>)
+                .asSequence()
+                .drop(1)
+                .flatMap {
+                    when (it) {
+                        is ChildListPropertyDescriptor -> (node.getStructuralProperty(it) as List<ASTNode>).asSequence()
+                        is SimplePropertyDescriptor -> emptySequence()
+                        else -> sequenceOf(node.getStructuralProperty(it) as ASTNode?)
+                    }
+                }
+                .filterNotNull()
+                .first()
     }
 }
--- src/main/kotlin/io/github/t45k/lvmutator/Validator.kt
+++ src/main/kotlin/io/github/t45k/lvmutator/Validator.kt
@@ -3,6 +3,8 @@ package io.github.t45k.lvmutator
 import java.io.File
 
 class Validator {
+    private val alreadyValidatedClonePair: MutableSet<Int> = mutableSetOf()
+
     fun validate(resultFileName: String) {
         val variance = Array(21) { 0 }
         File(resultFileName).readLines()
@@ -28,7 +30,10 @@ class Validator {
     private fun calc(base: String, mutant: String): Int {
         val baseNumber = getNumber(base)
         val mutantNumber = getNumber(mutant)
-        return if (mutantNumber > (baseNumber - 1) * 20 && mutantNumber <= baseNumber * 20) {
+        return if (mutantNumber > (baseNumber - 1) * 20 && mutantNumber <= baseNumber * 20
+            && !alreadyValidatedClonePair.contains(mutantNumber)
+        ) {
+            alreadyValidatedClonePair.add(mutantNumber)
             mutantNumber - (baseNumber - 1) * 20
         } else {
             0
